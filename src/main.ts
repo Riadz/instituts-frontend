6fd8e564f22fac13e89a30875f7782bfd56e3425
Riadz <riad.hachemane@gmail.com>
2021-07-28 21:00:43 +0100

    added main sass files + fonts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import App from '@/App.vue';
 import PrimeVue from 'primevue/config';
 import Router from '@/router';
 
+//
+import 'primevue/resources/themes/bootstrap4-light-blue/theme.css';
+import '@/styles/main.scss';
+
 const app = createApp(App).use(Router).use(PrimeVue);
 
 app.mount('#app');
