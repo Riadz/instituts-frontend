3c3cf64162307906092cf0cb2b13646eabe4fab9
Riadz <riad.hachemane@gmail.com>
2021-07-28 23:31:29 +0100

    added primevue style

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,9 @@ import Store from '@/store';
 
 // Styles
 import 'primevue/resources/themes/bootstrap4-light-blue/theme.css';
+import 'primevue/resources/primevue.min.css';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
 import '@/styles/main.scss';
 
 //
