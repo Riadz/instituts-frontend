4a1d8d8f0943c2efe5326d8d9b97aa31947a89ab
Riadz <riad.hachemane@gmail.com>
2021-07-28 21:01:22 +0100

    folder structure refactor

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
-import Index from '@/views/pages/Index.vue';
+import Index from '@/views/Index.vue';
 
 const routes: Array<RouteRecordRaw> = [
   { path: '/', name: 'index', component: Index },
---
+++ src/views/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <base-layout>
+    <h1>Hello world</h1>
+  </base-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import BaseLayout from '@/views/layouts/BaseLayout.vue';
+
+export default defineComponent({
+  setup() {},
+  components: {
+    BaseLayout,
+  },
+});
+</script>
