9ef97770a1083b267091b9a2d2ce812d190d12da
Riadz <riad.hachemane@gmail.com>
2021-08-07 13:37:51 +0100

    tweaked index page

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,15 +1,30 @@
 <template>
-  <base-layout>
-    <h1>Hello world</h1>
+  <base-layout mainClass="index">
+    <router-link :to="{ name: 'login' }">
+      <Button label="Se connecter" class="p-button-primary" />
+    </router-link>
+    <router-link :to="{ name: 'entry' }" class="p-ml-3">
+      <Button label="Crée un entrée" class="p-button-secondary" />
+    </router-link>
   </base-layout>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import BaseLayout from '@/views/layouts/BaseLayout.vue';
+//
+import Button from 'primevue/Button';
 
 export default defineComponent({
+  components: { BaseLayout, Button },
   setup() {},
-  components: { BaseLayout },
 });
 </script>
+
+<style lang="scss">
+main.index {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
