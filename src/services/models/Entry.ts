bbe2392bb2ed51cdbb2ad4752de98a4203bfd259
Riadz <riad.hachemane@gmail.com>
2021-08-14 12:25:55 +0100

    Entry + EntryData models functions

--- src/services/models/Entry.ts
+++ src/services/models/Entry.ts
@@ -1,7 +1,45 @@
 import config from '@/config';
 import req from '@/services/req';
+import EntryData from './EntryData';
+import Institute from './Institute';
 
 class Entry {
+  constructor(
+    public id: number,
+    public state: string,
+    public comment: string,
+    public institute_id: number,
+    public institute: Institute,
+    public data: Array<EntryData>,
+    public created_at: Date,
+    public updated_at: Date
+  ) {}
+
+  static fromJson(data: any) {
+    return new this(
+      data['id'],
+      data['state'],
+      data['comment'],
+      data['institute_id'],
+      Institute.fromJson(data['institute']),
+      EntryData.fromJsonArray(data['data']),
+      new Date(data['created_at']),
+      new Date(data['updated_at'])
+    );
+  }
+  static fromJsonArray(data_array: any) {
+    return (data_array as Array<Entry>).map((data) => this.fromJson(data));
+  }
+  static async all(state = 'pending') {
+    try {
+      let res = await req.get(`${config.apiUrl}/entry`, {
+        params: { state },
+      });
+      return this.fromJsonArray(res.data.data);
+    } catch (error) {
+      throw new Error('institute all error: ' + error);
+    }
+  }
   static async create(data) {
     try {
       let res = await req.post(`${config.apiUrl}/entry`, data);
--- src/services/models/EntryData.ts
+++ src/services/models/EntryData.ts
@@ -1,7 +1,38 @@
 import config from '@/config';
 import req from '@/services/req';
+import Formation from './Formation';
+import Specialty from './Specialty';
 
 class EntryData {
+  constructor(
+    public id: number,
+    public quantity: number,
+    public branch_code: string,
+    public specialty_id: number,
+    public specialty: Specialty,
+    public formation_id: number,
+    public formation: Formation,
+    public created_at: Date,
+    public updated_at: Date
+  ) {}
+
+  static fromJson(data: any) {
+    return new this(
+      data['id'],
+      data['quantity'],
+      data['branch_code'],
+      data['specialty_id'],
+      Specialty.fromJson(data['specialty']),
+      data['formation_id'],
+      Formation.fromJson(data['formation']),
+      new Date(data['created_at']),
+      new Date(data['updated_at'])
+    );
+  }
+  static fromJsonArray(data_array: any) {
+    return (data_array as Array<EntryData>).map((data) => this.fromJson(data));
+  }
+
   // prettier-ignore
   static branch_codes = [
     'ACP', 'AGR', 'AIG', 'ART',
