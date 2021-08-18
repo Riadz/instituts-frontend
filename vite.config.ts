050935d2c18c20d3a20b3e605c7e32171802807a
Riadz <riad.hachemane@gmail.com>
2021-08-19 00:25:12 +0100

    removed unused import

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import path from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import viteComponents, { PrimeVueResolver } from 'vite-plugin-components';
 
 export default defineConfig({
   plugins: [
